8712933a0fd3004aac99bb1cdc42176e93d5484a
Sandeep Joshi <sandeepjoshi@Sandeeps-MacBook-Pro.local>
2015-11-28 15:34:13 +0530

    add basic tests for user group

--- test/user.js
+++ test/user.js
@@ -2,10 +2,18 @@ var localStorage = require('./localStorage');
 
 var assert = require('assert');
 var expect = require('expect');
-var UserActions = require('../app/scripts/actions/userActions');
-var UserStore = require('../app/scripts/stores/userStore');
+import UserActions from '../app/scripts/actions/userActions';
+import UserStore from '../app/scripts/stores/userStore';
+
+var unsubscribe;
 
 describe('User Store', function() {
+
+  afterEach(function () {
+      if (unsubscribe)
+          unsubscribe();
+  });
+
 
   it("is configured", function () {
     // expect(UserStore.listenables).to.include(UserActions);
@@ -13,17 +21,56 @@ describe('User Store', function() {
   });
 
   it('should contain newly added item', function () {
-    UserStore.default.onAddItem({'name':'sandy', 'group' : [  ]});
-    assert.equal(1, UserStore.default.totalUserItems.length);
-    assert.equal('sandy', UserStore.default.totalUserItems[0].name);
+    UserStore.onAddItem({'name':'sandy', 'group' : [ { name : 'group 1' , id : 1 } ]});
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal('sandy', UserStore.totalUserItems[0].name);
 
   });
 
-  it('should delete item', function () {
-    assert.equal(1, UserStore.default.totalUserItems.length);
-    assert.equal(1, UserStore.default.totalUserItems[0].id);
-    UserStore.default.onDeleteItem(1);
-    assert.equal(0, UserStore.default.totalUserItems.length);
+  it('should trigger details of required user', function () {
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal(1, UserStore.totalUserItems[0].id);
+
+    unsubscribe = UserStore.listen(function (keys) {
+      assert.equal(1, keys.userId);
+    });
+
+    UserStore.onGetDetails(1);
+
   });
+
+  it('should delete group from user', function () {
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal(1, UserStore.totalUserItems[0].id);
+    assert.equal(1, UserStore.totalUserItems[0].group.length);
+    assert.equal(1, UserStore.totalUserItems[0].group[0].id);
+
+    UserStore.onDeleteGroupFromUser(1,1);
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal(0, UserStore.totalUserItems[0].group.length);
+
+  });
+
+  it('should add group to user', function () {
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal(1, UserStore.totalUserItems[0].id);
+    assert.equal(0, UserStore.totalUserItems[0].group.length);
+
+    UserStore.onAddGroupToUser({ userId : 1 , name : 'group 2' , groupId : 2});
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal(1, UserStore.totalUserItems[0].group.length);
+    assert.equal('group 2', UserStore.totalUserItems[0].group[0].name);
+
+  });
+
+
+  it('should delete user', function () {
+    assert.equal(1, UserStore.totalUserItems.length);
+    assert.equal(1, UserStore.totalUserItems[0].id);
+    UserStore.onDeleteItem(1);
+    assert.equal(0, UserStore.totalUserItems.length);
+  });
+
 
 });
+
